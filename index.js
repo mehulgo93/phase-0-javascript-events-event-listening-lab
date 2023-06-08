1b027e20bf9338ff2973700547a0023405740568
Mehul <mehul_angle@hotmail.com>
2023-06-08 16:04:59 -0400

    JS events

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// function addingEventListener() {
+// const input = document.getElementById('button');
+
+//   function clickAlert() {
+//     // Handle the event here
+//     alert('I was clicked');
+//   }
+// };
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
 }
